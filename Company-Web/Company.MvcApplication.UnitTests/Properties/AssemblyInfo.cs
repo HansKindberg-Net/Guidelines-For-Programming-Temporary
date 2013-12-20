65cf97dd9fb9da7d3173a3ab3b55c211af234ddc
Hans Kindberg <hans.kindberg@gmail.com>
2013-12-20 08:15:33 +0100

    Code backup

---
+++ Company-Web/Company.MvcApplication.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("Unit-tests for Company.MvcApplication.")]
+[assembly: CLSCompliant(true)]
+[assembly: Guid("06de7792-13ea-49bb-b768-25d1a81cd91f")]
+// ReSharper disable CheckNamespace
+internal static class AssemblyInfo
+// ReSharper restore CheckNamespace
+{
+	#region Fields
+
+	internal const string AssemblyName = "Company.MvcApplication.UnitTests";
+
+	#endregion
+}
---
+++ Company-Web/Company.MvpApplication.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("Unit-tests for Company.MvpApplication.")]
+[assembly: CLSCompliant(true)]
+[assembly: Guid("8319412f-40d0-431c-8ece-0d313ff5b245")]
+// ReSharper disable CheckNamespace
+internal static class AssemblyInfo
+// ReSharper restore CheckNamespace
+{
+	#region Fields
+
+	internal const string AssemblyName = "Company.MvpApplication.UnitTests";
+
+	#endregion
+}
