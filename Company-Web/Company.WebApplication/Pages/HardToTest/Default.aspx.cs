aa5425b096671417823b0f0bf0ddc46761d62b31
Hans Kindberg <hans.kindberg@gmail.com>
2014-01-04 17:49:41 +0100

    Code backup

--- Company-Web/Company.WebApplication/Pages/HardToTest/Default.aspx.cs
+++ Company-Web/Company.WebApplication/Pages/HardToTest/Default.aspx.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace Company.WebApplication.Pages.HardToTest
 {
 	public partial class Default : System.Web.UI.Page
 	{
-		protected void Page_Load(object sender, EventArgs e)
-		{
+		#region Methods
 
-		}
+		protected void Page_Load(object sender, EventArgs e) {}
+
+		#endregion
 	}
 }
