dba58100f5736083577a81b2ed0f065042b6eafd
Hans Kindberg <hans.kindberg@gmail.com>
2014-03-27 09:29:25 +0100

    Fix

--- Company-Examples/Company.Examples.UnitTests/Testability/HardToTest/ClassWithDirectoryEntryDependencyTest.cs
+++ Company-Examples/Company.Examples.UnitTests/Testability/HardToTest/ClassWithDirectoryEntryDependencyTest.cs
@@ -36,16 +36,20 @@ namespace Company.Examples.UnitTests.Testability.HardToTest
 			// We test something that depends on data that we do not have control over. If more properties are added to the data source (directory) the test will fail.
 			// If this test throws an exception a firewall is probably blocking the traffic (port 389).
 
-			IEnumerable<string> propertyNames = new ClassWithDirectoryEntryDependency {Condition = false}.GetLdapRootPropertyNames().ToArray();
+			IEnumerable<string> propertyNames = new ClassWithDirectoryEntryDependency {Condition = true}.GetLdapRootPropertyNames().ToArray();
 
-			Assert.AreEqual(6, propertyNames.Count());
+			Assert.AreEqual(10, propertyNames.Count());
 
-			Assert.AreEqual("objectclass", propertyNames.ElementAt(0));
-			Assert.AreEqual("supportedextension", propertyNames.ElementAt(1));
-			Assert.AreEqual("supportedcontrol", propertyNames.ElementAt(2));
-			Assert.AreEqual("supportedsaslmechanisms", propertyNames.ElementAt(3));
-			Assert.AreEqual("supportedldapversion", propertyNames.ElementAt(4));
-			Assert.AreEqual("dataversion", propertyNames.ElementAt(5));
+			Assert.AreEqual("objectClass", propertyNames.ElementAt(0));
+			Assert.AreEqual("namingContexts", propertyNames.ElementAt(1));
+			Assert.AreEqual("supportedExtension", propertyNames.ElementAt(2));
+			Assert.AreEqual("supportedControl", propertyNames.ElementAt(3));
+			Assert.AreEqual("supportedSASLMechanisms", propertyNames.ElementAt(4));
+			Assert.AreEqual("supportedLDAPVersion", propertyNames.ElementAt(5));
+			Assert.AreEqual("vendorName", propertyNames.ElementAt(6));
+			Assert.AreEqual("vendorVersion", propertyNames.ElementAt(7));
+			Assert.AreEqual("dataversion", propertyNames.ElementAt(8));
+			Assert.AreEqual("netscapemdsuffix", propertyNames.ElementAt(9));
 		}
 
 		[TestMethod]
